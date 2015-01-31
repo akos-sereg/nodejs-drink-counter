a0acfd2d65f30b92f2eea1f8e30c8669339aaf52
akos.sereg <akos.sereg@gmail.com>
2015-01-31 18:36:33 +0100

    Removing unused functions

--- public/mobile/js/drink-counter.js
+++ public/mobile/js/drink-counter.js
@@ -1,24 +1,3 @@
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-Date.prototype.yyyy_mm_dd = function() {
-   var yyyy = this.getFullYear().toString();
-   var mm = (this.getMonth()+1).toString();
-   var dd  = this.getDate().toString();
-   return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
-};
-
-Date.prototype.ddmmyyyy = function() {
-   var yyyy = this.getFullYear().toString();
-   var mm = (this.getMonth()+1).toString();
-   var dd  = this.getDate().toString();
-   return (dd[1]?dd:"0"+dd[0]) + '.' + (mm[1]?mm:"0"+mm[0]) + '.' + yyyy;
-};
-
 var username = 'akoss';
 
 var venueController = new VenueController(new VenueView());
